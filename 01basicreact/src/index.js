6920d775d88d55c872d39e0c2c5618f1458c059f
Munavar94 <munavarsheik94@gmail.com>
2024-01-03 15:24:29 +0530

    javascript written in html

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode> // for Developement 
+    <App />             // custom JS render with the help of JSX in HTML
+  // </React.StrictMode>
 );
