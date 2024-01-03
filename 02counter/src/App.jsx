cb73a5563ad177fc7e60f83d00b91ca9ff5b2aec
Munavar94 <munavarsheik94@gmail.com>
2024-01-03 22:31:15 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => {
+    console.log('value added', Math.random())
+    setCount((prev) => (
+      prev + 1
+    ))
+  }
+
+  const handleRemove = () => {    
+    setCount((prev) => (
+      prev > 0 ? prev - 1 : 0
+    ))
+  }
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Counter Value: {count}</h2>
+
+      <button onClick={handleAdd}>Add</button>
+      <br />
+      <button onClick={handleRemove}>Remove</button>
+    </>
+  )
+}
+
+export default App
