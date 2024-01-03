5d284deb1f78884f6f3ddf587e4ef578db5c3ee7
Munavar94 <munavarsheik94@gmail.com>
2024-01-03 21:53:16 +0530

    function convert to object

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'some text'
+// };
+
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank' rel="noreferrer">
+    visit Google
+  </a>
+)
+console.log(anotherElement);  // finally it convert to object
+
+// $$typeof :  Symbol(react.element)
+// key :  null
+// props :  {href: 'https://google.com', target: '_blank', rel: 'noreferrer', children: 'visit Google'}
+// ref :  null
+// type : "a"
+// _source :  {fileName: 'E:/01 React/chai_aur_react/01vitereact/src/main.jsx', lineNumber: 17, columnNumber: 1}
+// [[Prototype]] :  Object
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    // <App />
+
+    anotherElement
+  
 )
